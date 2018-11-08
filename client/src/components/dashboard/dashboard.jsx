e149f7868764b91684f72fdd8047548a4a051c53
James.Ngo <jamesngo125@yahoo.com>
2018-11-07 20:53:06 -0800

    dashboardcss

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -1,162 +1,48 @@
 
 import React, { Component } from 'react';
+import Board from 'react-trello';
 import "./dashboard.css";
 
-// const MOCK_DATA = {
-//     incomplete: [
-//         {
-//             name: 'Chore #4'
-//         },
-//         {
-//             name: 'Chore #5'
-//         }
-//     ],
-//     progress: [
-//         {
-//             name: 'Chore #1'
-//         }
-//     ],
-//     complete: [
-//         {
-//             name: 'Chore #2'
-//         },
-//         {
-//             name: 'Chore #3'
-//         },
-//         {
-//             name: 'Chore #5'
-//         }
-//     ],
-// };
-
-
-const MOCK_ROOMMATES_DATA = [
-    {
-        userId: 'some_id',
-        name: 'John Doe',
-        chores: {
-            incomplete: [
-                {
-                    name: 'Chore #4'
-                },
-                {
-                    name: 'Chore #5'
-                }
-            ],
-            progress: [
-                {
-                    name: 'Chore #1'
-                }
-            ],
-            complete: [
-                {
-                    name: 'Chore #2'
-                },
-                {
-                    name: 'Chore #3'
-                },
-                {
-                    name: 'Chore #5'
-                }
-            ],
-        }
-    },
-    {
-        userId: 'some_id',
-        name: 'Jane Doe',
-        chores: {
-            incomplete: [
-                {
-                    name: 'Chore #1'
-                },
-                {
-                    name: 'Chore #5'
-                }
-            ],
-            progress: [],
-            complete: [
-                {
-                    name: 'Chore #2'
-                },
-                {
-                    name: 'Chore #3'
-                }
-            ],
-        }
-    },
-];
-
-function ChoreColumn(props) {
-    const { className, data } = props;
-    if (!data) return null;
-
-    return (
-        <div className="progresscol">
-            {data.map(chore => (
-                <div>
-                    <h2>{chore.name}</h2>
-                </div>
-            ))}
-        </div>
-    )
-}
+const data = {
+    lanes: [
+      {
+        id: 'lane1',
+        title: 'Unassigned',
+        label: '2/2',
+        cards: [
+          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
+          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane2',
+        title: 'Assigned',
+        label: '0/0',
+        cards: []
+      },
+      {
+        id: 'lane3',
+        title: 'Completed',
+        label: '0/0',
+        cards: []
+      }
+    ]
+};
+ 
+  const styled = {
+      background: 'none',
+      width: '100vw',
+     
+  };
 
 class Dashboard extends Component {
-    componentDidMount() {
-        // axios.get('/api/user/getAllChores')
-        // .then(data => {
-        //     this.setState({ data: data });
-        // })
-
-        this.setState({ roommates: MOCK_ROOMMATES_DATA });
-    }
-
-    state = {
-        roommates: []
-    };
-
     render() {
-        const { roommates } = this.state;
-
-        console.log(roommates);
         return (
-            <div> 
-                <h1>My dashboard</h1>
-                {   
-                    roommates.length && roommates.map(roommate => {
-                        const { incomplete, progress, complete } = roommate.chores;
-                        return (
-                            <div className="chore-wrapper">
-                                <ChoreColumn className="incompletecol" data={incomplete} />
-                                <ChoreColumn className="prorgresscol" data={progress} />
-                                <ChoreColumn className="completecol" data={complete} />
-                            </div>
-                        )
-                    })
-                }
-                {/* <div className="chore-wrapper">
-                    <ChoreColumn className="incompletecol" data={incomplete} />
-                    <ChoreColumn className="prorgresscol" data={progress} />
-                    <ChoreColumn className="completecol" data={complete} />
-        
-                </div>
-
-                <div className="chore-wrapper">
-                    <ChoreColumn className="incompletecol" data={incomplete} />
-                    <ChoreColumn className="prorgresscol" data={progress} />
-                    <ChoreColumn className="completecol" data={complete} />
-        
-                </div>
-
-                <div className="chore-wrapper">
-                    <ChoreColumn className="incompletecol" data={incomplete} />
-                    <ChoreColumn className="prorgresscol" data={progress} />
-                    <ChoreColumn className="completecol" data={complete} />
-        
-                </div> */}
-
+            <div>
+                <h1> Chores to Do </h1>
+            <Board data={data} draggable style={styled} />
             </div>
-        )
+        );
     }
 }
 
