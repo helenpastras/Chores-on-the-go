16e74b36294a4d8b7298215df421c1ccde1150e5
James.Ngo <jamesngo125@yahoo.com>
2018-11-07 21:58:48 -0800

    mock data

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -10,7 +10,7 @@ const data = {
         title: 'Unassigned',
         label: '2/2',
         cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
+          {id: 'Card1', title: 'Wash Dishes', description: 'Can AI make memes', label: '30 mins'},
           {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
         ]
       },
